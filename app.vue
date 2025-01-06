74d8ddc41b3cfa36498500a4a528eb6e6e18f042
Chuian <chuiantw1212@gmail.com>
2025-01-06 20:13:21 +0800

    add gitignore

--- app.vue
+++ app.vue
@@ -8,3 +8,26 @@
     </el-footer>
   </el-container> -->
 </template>
+<script setup lang="ts">
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUjd-FjoB2jX5Hhm8gnOEyD-NLl1qwmRw",
+  authDomain: "enoch-259ad.firebaseapp.com",
+  projectId: "enoch-259ad",
+  storageBucket: "enoch-259ad.firebasestorage.app",
+  messagingSenderId: "180766333999",
+  appId: "1:180766333999:web:cdd879382f36205a04bada",
+  measurementId: "G-05HZ6B2YC0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+</script>
